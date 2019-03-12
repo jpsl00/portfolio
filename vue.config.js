c1709238dfea069fb913e350f993e43564170632
jpsl00 <joaopedroluciano@gmail.com>
2019-03-12 16:22:43 -0300

    :bug: _redirects fix

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  assetsDir: undefined,
-  publicPath: undefined,
-  outputDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined
-}
