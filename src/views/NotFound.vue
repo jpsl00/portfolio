a29d5c01959f4647ba8df17767809e112506a932
jpsl00 <joaopedroluciano@gmail.com>
2019-03-13 20:01:44 -0300

    :lipstick: Changing 404 text & icon color

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,18 +1,18 @@
 <template>
-  <section class="hero is-dark is-fullheight-with-navbar">
+  <section class="hero is-dark is-bold is-fullheight-with-navbar">
     <div class="hero-body">
       <div class="container">
         <div class="columns is-vcentered is-centered">
           <div class="column box is-narrow">
             <article class="media">
               <!-- <figure class="media-left"> -->
-                <span class="media-left icon is-large has-text-warning">
+                <span class="media-left icon is-large has-text-danger">
                   <fa icon="exclamation-circle" size="2x"/>
                 </span>
               <!-- </figure> -->
               <div class="media-content">
                 <div class="field">
-                  <h2 class="title is-2 has-text-warning">
+                  <h2 class="title is-2 has-text-danger">
                     404
                   </h2>
                   <h5 class="subtitle is-5 has-text-dark">
